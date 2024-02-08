495482198a3a93c85fe27eaf0f0ea579018aa211
MarcinRejniak <120187043+MarcinRejniak@users.noreply.github.com>
2024-02-08 20:09:09 +0100

    first commit

--- src/main/java/com/logic/Main.java
+++ src/main/java/com/logic/Main.java
@@ -8,6 +8,7 @@ import util.ConnectionUtilInterface;
 import java.sql.SQLException;
 
 
+
 public class Main {
     public static void main(String[] args) throws SQLException, InterruptedException {
         ConnectionUtilInterface connectionUtilInterface = new ConnectionUtil();
